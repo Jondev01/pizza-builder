05d0d27db814c6c6bf8feb173b6f5348d5e31012
Jonathan Wernersson <jonathan2008@live.se>
2019-02-11 21:58:29 +0100

    added Pizzabuilder component

---
+++ src/containers/PizzaBuilder/PizzaBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Auxillary from '../../hoc/Auxillary';
+
+class PizzaBuilder extends Component {
+    render () {
+        return (
+            <Auxillary>
+                <div>Pizza</div>
+                <div>Build Controls</div>
+            </Auxillary>
+
+        );
+    }
+}
+
+export default PizzaBuilder;
