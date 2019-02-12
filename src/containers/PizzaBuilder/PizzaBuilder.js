48644dcc24ade71185d9a0542d5478fe349c36bf
Jonathan Wernersson <jonathan2008@live.se>
2019-02-12 08:17:56 +0100

    addIngredientHandler

--- src/containers/PizzaBuilder/PizzaBuilder.js
+++ src/containers/PizzaBuilder/PizzaBuilder.js
@@ -3,6 +3,12 @@ import Auxillary from '../../hoc/Auxillary';
 import Pizza from '../../components/Pizza/Pizza';
 import BuildControls from '../../components/Pizza/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
 class PizzaBuilder extends Component {
     state = {
         ingredients : {
@@ -10,13 +16,30 @@ class PizzaBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice+priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    }
+
+
     render () {
         return (
             <Auxillary>
                 <Pizza ingredients = {this.state.ingredients} />
-                <BuildControls />
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler} />
             </Auxillary>
 
         );
