5f96318600c8579979595c0a0a649446855e5cc6
Jonathan Wernersson <jonathan2008@live.se>
2019-02-12 07:42:03 +0100

    added no-ingredients check

--- src/components/Pizza/Pizza.js
+++ src/components/Pizza/Pizza.js
@@ -3,12 +3,18 @@ import classes from './Pizza.module.css';
 import PizzaIngredient from './PizzaIngredient/PizzaIngredient';
 
 const pizza = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingKey => {
             return [...Array(props.ingredients[ingKey])].map((_, i) => {
                 return <PizzaIngredient key={ingKey+i} type={ingKey} />
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className = {classes.Pizza}>
             <PizzaIngredient type="bread-top" />
