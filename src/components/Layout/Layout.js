e0a9d80638a2edb770a9c4993c0036ec8200328d
Jonathan Wernersson <jonathan2008@live.se>
2019-02-13 20:26:17 +0100

    added backdrop to sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {Component } from 'react';
 import Auxillary from '../../hoc/Auxillary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Auxillary>
-        <Toolbar />
-        <SideDrawer />
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className = {classes.Content}>
-            {props.children}
-        </main>
-    </Auxillary>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+
+    }
+    render() {
+        return (
+            <Auxillary>
+                <Toolbar />
+                <SideDrawer 
+                    closed={this.sideDrawerClosedHandler}
+                    open={this.state.showSideDrawer} />
+                <div>Toolbar, SideDrawer, Backdrop</div>
+                <main className = {classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxillary>
+        );
+    }
+}
+
+export default Layout;
