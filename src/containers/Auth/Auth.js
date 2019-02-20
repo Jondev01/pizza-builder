70a17c426a5e68c6f763183ad818ce24e045afb5
Jonathan Wernersson <jonathan2008@live.se>
2019-02-20 21:53:27 +0100

    added signin on firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -36,7 +36,8 @@ class Auth extends Component {
                 valid: false,
                 touched: false
             },
-        }
+        },
+        isSignup: true
     };
 
     checkValidity(value, rules) {
@@ -70,7 +71,13 @@ class Auth extends Component {
 
     submitHandler = (event) => {
         event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
+    }
+
+    switchAuthModeHandler = () => {
+        this.setState( prevState => {
+            return {isSignup: !prevState.isSignup}
+        });
     }
 
 
@@ -99,6 +106,7 @@ class Auth extends Component {
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
+                <Button btnType="Danger" clicked={this.switchAuthModeHandler}>SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}</Button>
             </div>
         );
     }
@@ -107,7 +115,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password))
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
     };
 }
 
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,15 +21,18 @@ export const authFail = (error) => {
     };
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
             email: email,
             password: password,
             returnSecureToken: true
-        }
-        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDIsnVi6-PANpMVD9yo1oM2-BR2HaSQJ2s', authData)
+        };
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDIsnVi6-PANpMVD9yo1oM2-BR2HaSQJ2s';
+        if(!isSignup)
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDIsnVi6-PANpMVD9yo1oM2-BR2HaSQJ2s';
+        axios.post(url, authData)
         .then( response => {
             console.log(response);
             dispatch(authSuccess(response.data));
